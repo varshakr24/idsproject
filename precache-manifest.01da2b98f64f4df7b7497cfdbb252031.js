ab9ef44d5adac6ae25e64a77fb411742c04f4a15
Varsha Kuppur Rajendra <vkuppurr@andrew.cmu.edu>
2019-12-05 21:04:44 -0500

    Updates

---
+++ precache-manifest.01da2b98f64f4df7b7497cfdbb252031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c329cf50d4eee66d7f102d5fcab6394c",
+    "url": "/idsproject/index.html"
+  },
+  {
+    "revision": "c2be17ef60dd64e0f33d",
+    "url": "/idsproject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/idsproject/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/idsproject/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2be17ef60dd64e0f33d",
+    "url": "/idsproject/static/js/main.46ed43f7.chunk.js"
+  },
+  {
+    "revision": "70dcc617ed94057b6080",
+    "url": "/idsproject/static/js/runtime-main.7243b5ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/idsproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
